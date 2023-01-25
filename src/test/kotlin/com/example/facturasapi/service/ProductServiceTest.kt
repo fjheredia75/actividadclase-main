f7423a8011985cd082d67a54a015b1e09fbaa478
fjheredia75 <116654492+fjheredia75@users.noreply.github.com>
2023-01-24 18:15:39 -0600

    Initial commit

---
+++ src/test/kotlin/com/example/facturasapi/service/ProductServiceTest.kt
@@ -0,0 +1,57 @@
+package com.example.facturasapi.service
+
+import com.example.facturasapi.model.Product
+import com.example.facturasapi.repository.ProductRepository
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProductServiceSave {
+    @InjectMocks
+    lateinit var productService: ProductService
+
+    @Mock
+    lateinit var productRepository: ProductRepository
+
+    val productMock = Product().apply {
+        id= 1
+        description= "2"
+        brand= "toyota"
+        stock = 90
+        price= 850.0
+    }
+
+    @Test
+    fun saveProductCorrect(){
+        Mockito.`when`(productRepository.save(Mockito.any(Product::class.java))).thenReturn(productMock)
+        val response = productService.save(productMock)
+        Assertions.assertEquals(response.id, productMock.id)
+    }
+
+
+    @Test
+    fun saveProductWhenFullnameIsBlank(){
+
+        Assertions.assertThrows(Exception::class.java) {
+            productMock.apply { description=" "}
+            Mockito.`when`(productRepository.save(Mockito.any(Product::class.java))).thenReturn(productMock)
+            productService.save(productMock)
+        }
+
+    }
+
+    @Test
+    fun saveProductWhenStockIsZero(){
+
+        Assertions.assertThrows(Exception::class.java) {
+            productMock.apply { stock= 0}
+            Mockito.`when`(productRepository.save(Mockito.any(Product::class.java))).thenReturn(productMock)
+            productService.save(productMock)
+        }
+
+    }
+}
